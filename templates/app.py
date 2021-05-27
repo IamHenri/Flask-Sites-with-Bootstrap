9e7138b01c45a716721f2eb7a75de231d349dcf8
IamHenri <1054492+IamHenri@users.noreply.github.com>
2021-05-27 16:50:43 +0200

    error - delete

--- templates/app.py
+++
@@ -1,6 +0,0 @@
-#Source : https://iq.opengenus.org/single-page-application-with-flask-ajax/
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return render_template("index.html")
